d3b0d940d3cde0867d9973d53bee2b37d688cf22
AnvayRaj <78645037+AnvayRaj@users.noreply.github.com>
2022-06-09 00:11:46 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:csi_app/Home1.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(const MaterialApp(
+void main() {
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: home_1(),
+    home: AnimatedSplashScreen(
+        backgroundColor: Colors.deepPurple.shade400,
+        splashIconSize: 400,
+        splash: Container(
+          child: Image.asset(
+            'assets/csi logo.png',
+            height: 400,
+            width: 400,
+          ),
+        ),
+        nextScreen: home_1()),
   ));
 }
-
